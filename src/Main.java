6b61abefdefef78011cf0c3266ac62964f515112
Taha Sari <saritaha1907@gmail.comgit config --global user.email saritaha1907@gmail.comgit config --global user.email saritaha1907@gmail.com>
2022-10-31 00:26:58 +0300

    commit2-bugsfixed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void pow(){
+        int number=1,base;
+        double radix;
+        Scanner sacnner=new Scanner(System.in);
+
+     while(number!=0){
+         System.out.println("Please enter a radix(set radix 0 to end the loop) :");
+         number= sacnner.nextInt();
+         System.out.println("Please enter a base :");
+         base= sacnner.nextInt();
+         radix=Math.pow(number,base);
+         System.out.println("result="+radix);
+
+     }System.out.println("The cycle is over");
+
+
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        pow();
     }
 }
